9f8c4f61a92b5c83b203ce345928bc1c92103752
Raphael8119-maker <latelierdececilearthes@gmail.com>
2025-04-29 11:51:26 +0200

    Update Rng.py

--- Rng.py
+++ Rng.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import random
 import time
+import tkinter.font as font
 
 unit = 100
 listeCompetence = ["competence1","competence2","competence3"]
@@ -12,18 +13,25 @@ fenetre.geometry('1280x720')
 fenetre.resizable(width=False , height=False)
 
 def nouveauLancer():
+    global unit
     if unit >= 100 :
         for i in range(10):
             texte.set(random.choice(listeCompetence))
-            unit -= 100
-
+        unit -= 100
     else :
         texte.set("Pas assez d'unit.")
+        
+f = font.Font(size= 20)
 
-boutonLancer = Button(fenetre, text ='Lancer - Prix 100', command = nouveauLancer).place(x= 500 , y= 300)
+boutonLancer = Button(fenetre, text ='Lancer - Prix 100', command = nouveauLancer)
+boutonLancer['font'] = f
+boutonLancer.place(x= 620 , y= 370)
 
 texte = StringVar()
-labelLancer = Label(fenetre, textvariable = texte, fg ='Black', bg ='white').place(x= 500 , y= 280)
+texte.set("Bienvenue dans le shop de competence.")
+labelLancer = Label(fenetre, textvariable = texte, fg ='Black', bg ='white')
+labelLancer['font']=f
+labelLancer.place(x= 630 , y= 350)
 
 
 
