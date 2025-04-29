8b1ca4fd854aa22d9faa098ff474932b01c12995
Raphael8119-maker <latelierdececilearthes@gmail.com>
2025-04-29 10:39:20 +0200

    Update ArbreCompetence.py

--- ArbreCompetence.py
+++ ArbreCompetence.py
@@ -4,6 +4,31 @@ WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 WINDOW_TITLE = "ArbreCompetence - ClickLab"
 
+MOVEMENT_SPEED = 5
+
+SPRITE_SCALING = 0.5
+
+class Player(arcade.Sprite):
+    """ Player Class """
+
+    def update(self, delta_time: float = 1/60):
+        """ Move the player """
+        # Move player.
+        # Remove these lines if physics engine is moving player.
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        # Check for out-of-bounds
+        if self.left < 0:
+            self.left = 0
+        elif self.right > WINDOW_WIDTH - 1:
+            self.right = WINDOW_WIDTH - 1
+
+        if self.bottom < 0:
+            self.bottom = 0
+        elif self.top > WINDOW_HEIGHT - 1:
+            self.top = WINDOW_HEIGHT - 1
+
 class GameView(arcade.Window):
     
     def __init__(self):
@@ -11,18 +36,53 @@ class GameView(arcade.Window):
         super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
 
         self.background_color = arcade.csscolor.BLACK
+        
+        self.player_list = None
+        
+        self.player_sprite = None
     
     def setup(self):
-         pass
+         
+        self.player_list = arcade.SpriteList()
+
+        # Set up the player
+        self.player_sprite = Player(
+            ":resources:images/animated_characters/male_person/malePerson_idle.png",
+            scale=SPRITE_SCALING,
+        )
+        self.player_sprite.center_x = 50
+        self.player_sprite.center_y = 50
+        self.player_list.append(self.player_sprite)
         
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed. """
+
+        # If the player presses a key, update the speed
+        if key == arcade.key.UP:
+            self.player_sprite.change_y = MOVEMENT_SPEED
+        elif key == arcade.key.DOWN:
+            self.player_sprite.change_y = -MOVEMENT_SPEED
+        elif key == arcade.key.LEFT:
+            self.player_sprite.change_x = -MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT:
+            self.player_sprite.change_x = MOVEMENT_SPEED
+
     def on_draw(self):
        
        self.clear()
        
-    def main():
-        window = GameView()
-        window.setup()
-        arcade.run()
+       self.player_list.draw()
+
+    def on_key_release(self, key, modifiers):
+        if key == arcade.key.UP or key == arcade.key.DOWN:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.player_sprite.change_x = 0
+       
+def main():
+    window = GameView()
+    window.setup()
+    arcade.run()
         
 if __name__ == "__main__":
     main()
