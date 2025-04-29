e0faad248f7ee7e4afbb396499c17481ca1153de
Raphael8119-maker <latelierdececilearthes@gmail.com>
2025-04-29 10:49:16 +0200

    Update Rng.py

--- Rng.py
+++ Rng.py
@@ -1,1 +1,25 @@
-print("test")
+import arcade
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+WINDOW_TITLE = "Rng - ClickLab"
+
+class GameView(arcade.Window):
+    
+    def __init__(self):
+        
+        super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
+
+        self.background_color = arcade.csscolor.BLACK
+        
+    def on_draw(self):
+       
+       self.clear()
+       
+def main():
+    window = GameView()
+    window.setup()
+    arcade.run()
+        
+if __name__ == "__main__":
+    main()
