e250501a39ea12c4d446fd2375c1d6de8c7a45e2
Raphael8119-maker <latelierdececilearthes@gmail.com>
2025-04-29 11:35:37 +0200

    Update Rng.py

--- Rng.py
+++ Rng.py
@@ -1,25 +1,30 @@
-import arcade
+from tkinter import *
+import random
+import time
 
-WINDOW_WIDTH = 1280
-WINDOW_HEIGHT = 720
-WINDOW_TITLE = "Rng - ClickLab"
+unit = 100
+listeCompetence = ["competence1","competence2","competence3"]
 
-class GameView(arcade.Window):
-    
-    def __init__(self):
-        
-        super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
+# Création de la fenêtre principale (Main Window)
+fenetre = Tk()
+fenetre.title('Rng - ClickLab')
+fenetre.geometry('1280x720')
+fenetre.resizable(width=False , height=False)
 
-        self.background_color = arcade.csscolor.BLACK
-        
-    def on_draw(self):
-       
-       self.clear()
-       
-def main():
-    window = GameView()
-    window.setup()
-    arcade.run()
-        
-if __name__ == "__main__":
-    main()
+def nouveauLancer():
+    if unit >= 100 :
+        for i in range(10):
+            texte.set(random.choice(listeCompetence))
+            unit -= 100
+
+    else :
+        texte.set("Pas assez d'unit.")
+
+boutonLancer = Button(fenetre, text ='Lancer - Prix 100', command = nouveauLancer).place(x= 500 , y= 300)
+
+texte = StringVar()
+labelLancer = Label(fenetre, textvariable = texte, fg ='Black', bg ='white').place(x= 500 , y= 280)
+
+
+
+fenetre.mainloop()
