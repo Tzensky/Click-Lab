ff912296fc6c9aeeb5f133c16dfe5568d059c500
Raphael8119-maker <latelierdececilearthes@gmail.com>
2025-04-29 10:23:52 +0200

    Update ArbreCompetence.py

--- ArbreCompetence.py
+++ ArbreCompetence.py
@@ -1,2 +1,28 @@
 import arcade
 
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+WINDOW_TITLE = "ArbreCompetence - ClickLab"
+
+class GameView(arcade.Window):
+    
+    def __init__(self):
+
+        super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
+
+        self.background_color = arcade.csscolor.BLACK
+    
+    def setup(self):
+         pass
+        
+    def on_draw(self):
+       
+       self.clear()
+       
+    def main():
+        window = GameView()
+        window.setup()
+        arcade.run()
+        
+if __name__ == "__main__":
+    main()
