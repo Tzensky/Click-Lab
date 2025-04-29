6814ad549c502a8e28099314c6350063bb344f13
Raphael8119-maker <latelierdececilearthes@gmail.com>
2025-04-29 10:49:19 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,25 @@
-print("cc")
+import arcade
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+WINDOW_TITLE = "Main - ClickLab"
+
+class GameView(arcade.Window):
+    
+    def __init__(self):
+        
+        super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
+
+        self.background_color = arcade.csscolor.BLACK
+        
+    def on_draw(self):
+       
+       self.clear()
+       
+def main():
+    window = GameView()
+    window.setup()
+    arcade.run()
+        
+if __name__ == "__main__":
+    main()
